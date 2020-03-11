a7a006c13537b93006c2e0bf9a06befeb3c26cb5
karan.galgat <karan.galgat@digital.datamatics.com>
2020-03-11 12:54:26 +0530

    token neeeds to be created from front-end

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var cognitoAuth = require('./../lib/cognitoAuth')
+
+const cognitoAuthMiddleware = cognitoAuth.getVerifyMiddleware()
 
 /* GET users listing. */
-router.get('/', function (req, res, next) {
+router.get('/', cognitoAuthMiddleware, function (req, res, next) {
   res.status(200).json(req.user);
 });
 
