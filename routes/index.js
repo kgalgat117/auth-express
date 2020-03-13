91a88bdace433bc9d9d114553c4c72b49697b5c7
karan.galgat <karan.galgat@digital.datamatics.com>
2020-03-13 13:07:17 +0530

    working

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var request = require("request");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/token', function (req, res) {
+  let body = {
+    "client_id": 'oY45faai4l2bZrgJQMmQ4ezCO6YjWFbf',
+    "client_secret": '9C1JxboCOX37H-jrsL7Oxgg_uY-QEhbdEy_v3Kb2kxD_Lzipe3LeibPMSQF9ZSs-',
+    "audience": 'https://auth0-test-117.auth0.com/api/v2/',
+    "grant_type": 'client_credentials'
+  }
+  var options = {
+    method: 'POST',
+    url: 'https://auth0-test-117.auth0.com/oauth/token',
+    headers: { 'content-type': 'application/json' },
+    body: JSON.stringify(body)
+  };
+  request(options, function (error, response, body) {
+    if (error) {
+      res.status(400).json({ error: error })
+    } else {
+      res.status(200).json({ result: JSON.parse(body) })
+    }
+  })
+})
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var jwt = require('express-jwt');
+var jwks = require('jwks-rsa');
 
-/* GET users listing. */
-router.get('/', function (req, res, next) {
-  res.status(200).json(req.user);
+var jwtCheck = jwt({
+  secret: jwks.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: 'https://auth0-test-117.auth0.com/.well-known/jwks.json'
+  }),
+  audience: 'https://localhost:3000/users',
+  issuer: 'https://auth0-test-117.auth0.com/',
+  algorithms: ['RS256']
+});
+
+/* get users. */
+router.get('/', jwtCheck, function (req, res, next) {
+  res.status(200).json({ result: 'create user here', user: req.user });
+});
+
+/* get users. */
+router.get('/test', jwtCheck, function (req, res, next) {
+  res.status(200).json({ result: 'users test', user: req.user });
 });
 
 module.exports = router;
