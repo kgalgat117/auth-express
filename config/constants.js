dba07553e781d471f92d08268ee97310a4492f3e
karan.galgat <karan.galgat@digital.datamatics.com>
2020-03-15 15:36:52 +0530

    working

---
+++ config/constants.js
@@ -0,0 +1,7 @@
+var object = {
+    clientId: 'RIaj6be2Ai9eBm9Hy7z0fgegFiZR3UFS',
+    clientSecret: '9GyV9W11eH5wS8Zbvc7X5mMpVOLpv5wGFK1xt7Hyv4Xd7hdaZvnoJ3Clx3oRuzTA',
+    domain: 'auth0-test-117.auth0.com'
+}
+
+module.exports = object
---
+++ controller/nodeAuth.js
@@ -0,0 +1,86 @@
+var constants = require('./../config/constants')
+var auth0 = require('auth0')
+
+var AuthenticationClient = auth0.AuthenticationClient
+
+var authenticationClient = new AuthenticationClient({
+    clientId: constants.clientId,
+    clientSecret: constants.clientSecret,
+    domain: constants.domain
+})
+var authenticationClientToken = null
+var password = 'Password#123'
+// setAuthenticationClientToken()
+// userCreate()
+userSignIn()
+
+function userSignIn() {
+    return new Promise((resolve, reject) => {
+        let user = 'testuser6'
+        authenticationClient.passwordGrant({
+            username: user,
+            password: password,
+            realm: 'Username-Password-Authentication'
+        })
+            .then(response => {
+                console.log(response)
+                resolve(response)
+            }).catch(err => {
+                reject(err)
+            })
+    }).catch(e => {
+        console.log(e)
+    })
+}
+
+function userCreate() {
+    return new Promise((resolve, reject) => {
+        let user = 'testuser6'
+        authenticationClient.database.signUp({
+            email: `${user}@gmail.com`,
+            username: user,
+            password: 'Password#123',
+            connection: 'Username-Password-Authentication'
+        }).then(response => {
+            console.log(response)
+            resolve(response)
+        }).catch(err => {
+            reject(err)
+        })
+    }).catch(e => {
+        console.log(e)
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+function setAuthenticationClientToken() {
+    return new Promise((resolve, reject) => {
+        authenticationClient.clientCredentialsGrant({
+            audience: `https://${constants.domain}/api/v2/`
+        }, function (err, response) {
+            if (err) {
+                reject(err)
+            } else {
+                authenticationClientToken = response.access_token
+                resolve(response)
+            }
+        })
+    })
+}
+
+
+
+
+
+
